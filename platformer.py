77e322e0cdca561a15516ffd94a5b258a51116bb
achechet <achechet@gmail.com>
2022-02-08 17:55:43 +0300

    MK Coding - 3

--- platformer.py
+++ platformer.py
@@ -2,33 +2,78 @@
 from ursina import *
 from ursina.prefabs.platformer_controller_2d import PlatformerController2d
 
+def update():
+    global speed, dx
+    dx += speed*time.dt 
+    if abs(dx) > 2:
+        speed *= -1
+        dx = 0
+    for enemy in enemies:
+        enemy.x += speed*time.dt
+
+class Enemy(Entity):
+    def __init__(self, x, y):
+        super().__init__()
+        self.model = "cube"
+        self.texture = "assets/ghost.png"
+        self.color = color.green
+        self.x = x
+        self.y = y
+
 # Declare the app
+speed = 1
+dx = 0
 app = Ursina()
+
+enemies = []
+enemy = Enemy(1, 1)
+enemies.append(enemy)
+
+enemy = Enemy(-2.5-1, 2)
+enemies.append(enemy)
+
 size=13
+bg = Entity(model='quad', scale=(size,6), texture='assets/sky_cloud', z=1)
 
-bg = Entity(model="quad", scale=(size,6), texture="assets/sky_cloud", z=1)
-for m in range(2):
-    duplicate(bg, x=size*(m+1))
-    duplicate(bg, x=-size*(m+1))
     
-player = PlatformerController2d(y=1, z=.01, scale_y=1, color=color.green)
+player = PlatformerController2d(y=1, z=-.01, scale_y=1, color=color.white, texture='assets\guy.png')
 
-ground = Entity(model='quad', y=-2, scale_x=10, collider="box", color=color.yellow)
-duplicate(ground, x=size)
-duplicate(ground, x=-size)
+ground = Entity(model='quad', y=-2, scale_x=10, collider='box', color=color.yellow)
+
 
 wall = Entity(model='quad', color=color.azure, scale=(1,5), x=5.5, collider='box')
-duplicate(wall, x=size+5.5)
-duplicate(wall, x=5.5-size)
+
 
 level = Entity(model='quad', color=color.red, scale=(3,1), x=2, collider='box')
-duplicate(level, x=2+size)
-duplicate(level, x=2-size)
 
-ceiling = Entity(model="quad", color=color.cyan, scale=(3,1), x=-2.5, y=1, collider="box")
-duplicate(ceiling, x=-2.5+size)
-duplicate(ceiling, x=-2.5-size)
 
+ceiling = Entity(model='quad', color=color.cyan, scale=(3,1), x=-2.5, y=1, collider='box')
+
+trap = Entity(model='quad', scale=(2,1), x=-6, y=-2, texture='assets/trap.png', collider='box')
+
+extention = 1
+for m in range(extention):
+    enemy = Enemy(2-1+size*(m-1), 1)
+    enemies.append(enemy)
+    enemy = Enemy(2-1-size*(m-1), 1)
+    enemies.append(enemy)    
+    enemy = Enemy(-2.5-1+size*(m-1), 2)
+    enemies.append(enemy)
+    enemy = Enemy(-2.5-1-size*(m-1), 2)
+    enemies.append(enemy)       
+    duplicate(bg, x=size*(m+1))
+    duplicate(bg, x=-size*(m+1))
+    duplicate(ground, x=size*(m+1))
+    duplicate(ground, x=-size*(m+1))
+    duplicate(wall, x=5.5+size*(m+1))
+    duplicate(wall, x=5.5-size*(m+1))
+    duplicate(level, x=2+size*(m+1))
+    duplicate(level, x=2-size*(m+1))
+    duplicate(ceiling, x=-2.5+size*(m+1))
+    duplicate(ceiling, x=-2.5-size*(m+1))    
+    duplicate(trap, x=-6+size*(m+1))
+    duplicate(trap, x=-6-size*(m+1))    
+    
 camera.add_script(SmoothFollow(target=player, offset=[0, 1, -30], speed=4))
 # Run the app
 app.run()
